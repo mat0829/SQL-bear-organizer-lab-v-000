5cf7dcc74231050186ebb18ec634ce0c0ffa9abc
David Anderson <matandersonlv@hotmail.com>
2019-05-06 17:58:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,13 +2,13 @@ INSERT INTO bears (id, name)
     VALUES ("Mr. Chocolate");
 INSERT INTO bears (id, name)
     VALUES ("Rowdy");
-INSERT INTO bears (name)
+INSERT INTO bears (id, name)
     VALUES ("Tabitha");
-INSERT INTO bears (name)
+INSERT INTO bears (id, name)
     VALUES ("Sergeant Brown");
-INSERT INTO bears (name)
+INSERT INTO bears (id, name)
     VALUES ("Melissa");
-INSERT INTO bears (name)
+INSERT INTO bears (id, name)
     VALUES ("Grinch");
-INSERT INTO bears (name)
+INSERT INTO bears (id, name)
     VALUES (NULL);
